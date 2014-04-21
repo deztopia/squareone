4fc6eaa89e91a70683cdef93ee2f3c11c553e9b1
Jason Melendez <jason.melendez@gmail.com>
2014-04-21 13:51:50 -0700

    Add method to load collection from array, such as obtained from a stored procedure call

--- lib/ms/modelcollection.php
+++ lib/ms/modelcollection.php
@@ -72,6 +72,38 @@ class MsModelCollection implements Iterator, Countable {
 	 
 	 
 	/**
+	 * Load Array.
+	 * 
+	 * Loads this collection with model data stored as an array, normally obtained from a stored procedure call.
+	 * 
+	 * @parameter 	mixed[] 		$modelArrayIn Associative array of model data, where each row is a model to load.
+	 * @return void
+	 *
+	 */
+	 public function loadArray($modelArrayIn) {
+	 	// make sure class file has been included before we instantiate
+		$classNameArray = MsUtils::camelcaseToArray($this->classname);
+		$path = MS_PATH_BASE . DS . 'lib' . DS . strtolower($classNameArray[0]) . DS . strtolower($classNameArray[1]) . '.php';
+		if (file_exists($path)) require_once($path);
+		
+		// load each model into the collection
+	 	foreach ($modelArrayIn as $this_model) {
+		 	eval('$model = new ' . $this->classname . '($this->config, $this->db);');
+		 	if (is_object($model)) {
+		 		$model->setColumnValues($this_model);
+				$this->models[] = $model;
+		 	}
+	 	}
+	 	
+	 	// clear internal search index since we've loaded new data
+		if (sizeof($this->findMap) > 0) {
+			unset($this->findMap);
+			$this->findMap = array();
+		}
+	 }
+	 
+	 
+	/**
 	 * Load From Link Table.
 	 * 
 	 * Loads this collection with models based on references in a link table.
